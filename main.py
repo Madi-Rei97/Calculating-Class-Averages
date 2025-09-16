f6f57a84e8163677ab4d6533997f17bd8f393c2b
Madi Rei <madison.nicole.rei@gmail.com>
2025-09-16 11:05:31 -0700

    wrote calculate_class_averages function to pass all tests

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 def calculate_class_averages(classrooms):
-    pass
+    class_averages = {}
+
+    for subject, students in classrooms.items():
+        if not students:
+                class_averages[subject] = 0
+                continue
+
+        count_of_scores = 0
+        total_scores = 0
+
+        for scores in students.values():
+            count_of_scores += len(scores)
+
+            for score in scores:
+                total_scores += score
+
+        class_averages[subject] = total_scores / count_of_scores
+
+    return class_averages
